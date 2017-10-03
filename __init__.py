c0052bf029ea7bf6fa66ef3bac03bb16edf8d303
ayushmandey <ayushman.dey97@gmail.com>
2017-10-04 00:30:11 +0530

    Added article structure with dummy data

--- __init__.py
+++ __init__.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template
+from data import Articles
+
 app = Flask(__name__)
+Articles = Articles()
 
 @app.route('/')
 def index():
@@ -9,5 +12,13 @@ def index():
 def about():
 	return render_template('about.html')
 
+@app.route('/articles')
+def articles():
+	return render_template('articles.html', articles=Articles)
+
+@app.route('/article/<string:id>')
+def article(id):
+	return render_template('article.html', id=id)
+
 if __name__ == '__main__':
-	app.run(debug = True)
+	app.run(debug=True)
---
+++ data.py
@@ -0,0 +1,27 @@
+def Articles():
+	articles = [
+		{
+			'id': 1,
+			'title': 'Article 1',
+			'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 
+			'author':'Ayushman Dey',
+			'create_date':'04-25-17'
+		},
+		{
+			'id': 2,
+			'title': 'Article 2',
+			'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 
+			'author':'Dheeraj Lalwani',
+			'create_date':'05-13-17'
+		},
+		{
+			'id': 3,
+			'title': 'Article 3',
+			'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 
+			'author':'Raghu Tiwari',
+			'create_date':'02-12-17'
+		}
+
+	]
+
+	return articles
