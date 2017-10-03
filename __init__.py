0d1c28773e0fc32ad6e417729c6a73d8b2cbb5e1
ayushmandey <ayushman.dey97@gmail.com>
2017-10-03 23:54:18 +0530

    Completed 'Home' and 'About' interfaces

--- __init__.py
+++ __init__.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return '<h1> hello world! </h1>'
+	return render_template('home.html')
+
+@app.route('/about')
+def about():
+	return render_template('about.html')
 
 if __name__ == '__main__':
-	app.run()
+	app.run(debug = True)
